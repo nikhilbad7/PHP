4c7ec5461eef035a90e52062fda7115260980e2b
nikhil <nikhil.badani@gmail.com>
2018-03-03 13:55:34 +0530

    custom attributes of table inside echo

---
+++ 5.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<?php
+$name="nikhil";
+$total=100;
+$grade="A";
+echo "<table border=1><tr><td>NAME</td><td>$name</td></tr><tr><td>TOTAL</td><td>$total</td></tr><tr><td>GRADE</td><td>$grade</td></tr></table>"
+?>
+</body>
+</html>
