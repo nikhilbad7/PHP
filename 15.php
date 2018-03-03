9d5c179f88a666ba83f3c0895286e8b8a5cb0298
nikhil <nikhil.badani@gmail.com>
2018-03-03 14:56:46 +0530

    radio button

---
+++ 15.php
@@ -0,0 +1,8 @@
+//radio button use
+//radio signature=<input type="radio" name="x" value="male">male
+//name should be common
+<form action="16.php">
+<input type="radio" name="x" value="male">male;<br><br>
+<input type="radio" name="x" value="female">female;<br><br>
+<input type="submit" value="ok">
+</form>
