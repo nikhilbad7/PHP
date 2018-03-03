8e370dfc0555162f01a218332957cdbf59bb8506
nikhil <nikhil.badani@gmail.com>
2018-03-03 13:47:58 +0530

    table inside echo

---
+++ 3.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+$x=4;
+$y=5;
+$z=$x+$y;
+$m=$x*$y;
+echo "<table border=1><tr><td>$x</td><td>+</td><td>$y</td><td>=</td><td>$z</td></tr><tr><td>$x</td><td>*</td><td>$y</td><td>=</td><td>$m</td></tr></table>";
+?>
+</body>
+</html>
